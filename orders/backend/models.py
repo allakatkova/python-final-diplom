683dcb0ac785a3c7d1b54039705676715f317af8
Alla Katkova <allakatkova@bk.ru>
2023-06-29 20:14:59 +0500

    update project

--- orders/backend/models.py
+++ orders/backend/models.py
@@ -24,30 +24,33 @@ STATE_CHOICES = (
 class CustomUser(BaseUserManager):
     use_in_migrations = True
 
-    # общая модель создания пользователя
     def _create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
         if not email:
-            raise ValueError('The email must be set')
+            raise ValueError('The given email must be set')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
         return user
 
-    # создание покупателя/продавца
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
-        return self.create(email, password, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+
         if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have status is_staff=True')
+            raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have status is_superuser=True')
-        return self.create(email, password, **extra_fields)
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
--- orders/backend/views.py
+++ orders/backend/views.py
@@ -6,12 +6,8 @@ from backend.models import Shop, ShopFiles, Category, Product, ShopProduct, Para
 import yaml
 from orders.settings import BASE_DIR, DATA_ROOT
 import os
-import yaml
-from orders.settings import BASE_DIR, DATA_ROOT
-import os
 from django.contrib.auth.password_validation import validate_password
-from backend.serializers import UserSerializer, CategorySerializer, ShopSerializer, ProductSerializer, \
-    ShopProductSerializer, ProductInfSerializer, ContactSerializer
+from backend.serializers import UserSerializer, CategorySerializer, ShopSerializer, ProductSerializer, ShopProductSerializer, ProductInfSerializer, ContactSerializer
 from backend.signals import new_user_registered, new_order
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
